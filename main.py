a1df6f309707e4d63031eacb52b92409121e30d6
arsbul <ars.bulygin@yandex.ru>
2023-10-01 19:47:31 +0300

    dz complete

---
+++ main.py
@@ -0,0 +1,12 @@
+# a = "Текст" # здесь строка!
+# b = 10
+# h = float("20.5") + int("20")
+# print(h)
+#
+#
+# abc = input()
+# print(abc)
+
+a = float(input()) # тип данных float
+b = float(input()) # тип данных float
+print(a + b) # тип данных float
